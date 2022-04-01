6e88112383b0b08e150da0d825e3e48b26982a56
tomvieir <wevertonbvieira@gmail.com>
2022-04-01 20:57:40 -0300

    main pré montado

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,30 @@ import  Head from 'next/head'
 export default function Home() {
   return (
     <>
-      
+      <Head>
+        <title>Home | App.News</title>
+      </Head>
+
+      <main className={styles.Main}>
+        <section>
+          <h4>
+            hey welcome 
+          </h4>
+
+          <h1>News about <br />
+            the <span>React</span> world 
+          </h1>
+
+          <h5>Get acess to all publications <br />
+            <span>for $9,90 month</span>
+          </h5>
+
+          <button type="button"> Subscribe now</button>
+
+        </section>
+
+        <img src="/images/avatar2.svg" alt="coding img" />
+      </main>
     </>
   )
 }
--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,20 +1,22 @@
 import { FaGithub } from 'react-icons/fa';
-
+import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const  isUserLoggedIn = true
+    const  isUserLoggedIn = false;
 
 
     return isUserLoggedIn ? (
         <button type="button" className={styles.SignInButton}>
-            <FaGithub color='#eba417'/>
-            Entre com o Github
+            <FaGithub color='#17eb4c'/>
+            Tom Vieira
+            <FiX color='#727272' className={styles.SvgCloseIcon}/>
         </button>
     ) : (
         <button type="button" className={styles.SignInButton}>
-        <FaGithub color='#2ceb17'/>
-        Entre com o Github
-    </button>
+            <FaGithub color='#d8d20b' />
+            Entre com o Github
+            
+        </button>
     )
 }
