787ee53118992d16a76f9578893e0a3ef571a3de
tomvieir <wevertonbvieira@gmail.com>
2022-04-19 18:23:51 -0300

    Ambiente do conteúdo principal do post add

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,68 @@
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
+import Head from "next/head"
+import { RichText } from "prismic-dom"
+import { getPrismicClient } from "../../src/services/prismic"
+import styles from './post.module.scss'
+
+interface PostsProps {
+    post: {
+        title: string,
+        content: string,
+        slug: string,
+        updatedAt: string,
+    }
+}
+
+export default function Posts({ post }: PostsProps) {
+    return (
+        <>  
+            <Head>
+                <title>{post.title} | AppNews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <article className={styles.post}>
+                    <h1>{post.title}</h1>
+                    <time>{post.updatedAt}</time>
+                    <div 
+                       className={styles.postContent}
+
+                        dangerouslySetInnerHTML={{__html: post.content}} //prismic trata a vulnerabilidade nesse caso
+                    />
+
+                    
+                </article>
+            </main>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async ( { req, params }) => {
+    const session = await getSession( { req } )
+    const { slug } = params
+
+
+    const prismic = getPrismicClient(req)
+
+    const response = await prismic.getByUID<any>('my-custom-publication', String(slug), {})
+
+    const post = {
+        slug,
+        title: RichText.asText(response.data.title),
+        content: RichText.asHtml(response.data.content),
+        updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', { 
+            day: '2-digit',
+            month: 'long',
+            year:'numeric'
+        })
+    }
+    console.log(response)
+
+    return {
+        props: {
+            post,
+        }
+    }
+
+}
