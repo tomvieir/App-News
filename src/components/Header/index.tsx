e2fe35692d76364142a8745df4cb19dc9da3266e
tomvieir <wevertonbvieira@gmail.com>
2022-04-19 18:25:05 -0300

    Animação de seleção do Home|Post criado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
 import { SignInButton } from './SignInButton';
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Header() {
+    const { asPath } = useRouter()
+
     return (
     <header className={styles.headerContainer} >
         <div className={styles.headerContent}>
             <img src="/images/logo.svg" alt="Logo" />
 
             <nav>
-                <a href="http://localhost:3000/" className={styles.active}>Home</a>
-                <a href="">Posts</a>           
+                <Link href="/">
+                    <a className = {asPath === '/' ? styles.active : ''}>Home</a>
+                </Link>
+
+                <Link href="/posts" prefetch>
+                    <a className = {asPath === '/posts' ? styles.active : ''}>Posts</a>
+                </Link>
+                       
             </nav>
 
             <SignInButton />
