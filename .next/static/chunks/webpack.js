f9fdfa29dddb7501e0bdad2c5b71c177838736dc
tomvieir <wevertonbvieira@gmail.com>
2022-04-18 21:27:17 -0300

    Correção de tipagem

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c1e242de297df0d8"; }
+/******/ 		__webpack_require__.h = function() { return "6e648b09572f3e01"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
